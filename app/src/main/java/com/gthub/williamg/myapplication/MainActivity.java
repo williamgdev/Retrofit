9399ed87aaabb45c651010aa29e05ecc19074785
AndroidStorm <williamg.dev@gmail.com>
2017-02-16 11:52:20 -0600

    Create a simple Retrofit

--- app/src/main/java/com/gthub/williamg/myapplication/MainActivity.java
+++ app/src/main/java/com/gthub/williamg/myapplication/MainActivity.java
@@ -3,11 +3,25 @@ package com.gthub.williamg.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String BASE_URL = "https://api.cloud.wowza.com/api/v1/live_streams";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiService apiService = retrofit.create(ApiService.class);
+        apiService.getLiveStreamStatus()
+                .enqueue(new StreamStatusCallback());
     }
 }
